445cb86b4d2806e986d535e9c8fe7572736a58bc
Aqua <ollie@ollierobinson.co.uk>
2025-01-31 17:52:43 +0000

    vector2 tests covered

--- src/Euphoria.Math/Vector2T.cs
+++ src/Euphoria.Math/Vector2T.cs
@@ -1,8 +1,11 @@
 using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace Euphoria.Math;
 
-public struct Vector2T<T> :
+[StructLayout(LayoutKind.Sequential)]
+public readonly struct Vector2T<T> :
     IEquatable<Vector2T<T>>, 
     IEqualityOperators<Vector2T<T>, Vector2T<T>, bool>,
     IAdditionOperators<Vector2T<T>, Vector2T<T>, Vector2T<T>>, 
@@ -30,67 +33,84 @@ public struct Vector2T<T> :
         Y = y;
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public Vector2T<TOther> As<TOther>() where TOther : INumber<TOther>
+        => new Vector2T<TOther>(TOther.CreateChecked(X), TOther.CreateChecked(Y));
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public bool Equals(Vector2T<T> other)
     {
         return EqualityComparer<T>.Default.Equals(X, other.X) && EqualityComparer<T>.Default.Equals(Y, other.Y);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public override bool Equals(object? obj)
     {
         return obj is Vector2T<T> other && Equals(other);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public override int GetHashCode()
     {
         return HashCode.Combine(X, Y);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static bool operator ==(Vector2T<T> left, Vector2T<T> right)
     {
         return left.Equals(right);
     }
     
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static bool operator !=(Vector2T<T> left, Vector2T<T> right)
     {
         return !left.Equals(right);
     }
     
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static Vector2T<T> operator +(Vector2T<T> left, Vector2T<T> right)
     {
         return new Vector2T<T>(left.X + right.X, left.Y + right.Y);
     }
     
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static Vector2T<T> operator -(Vector2T<T> left, Vector2T<T> right)
     {
         return new Vector2T<T>(left.X - right.X, left.Y - right.Y);
     }
     
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static Vector2T<T> operator *(Vector2T<T> left, Vector2T<T> right)
     {
         return new Vector2T<T>(left.X * right.X, left.Y * right.Y);
     }
     
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static Vector2T<T> operator *(Vector2T<T> left, T right)
     {
         return new Vector2T<T>(left.X * right, left.Y * right);
     }
     
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static Vector2T<T> operator /(Vector2T<T> left, Vector2T<T> right)
     {
         return new Vector2T<T>(left.X / right.X, left.Y / right.Y);
     }
     
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static Vector2T<T> operator /(Vector2T<T> left, T right)
     {
         return new Vector2T<T>(left.X / right, left.Y / right);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public string ToString(string? format, IFormatProvider? formatProvider)
     {
         FormattableString formattable = $"{nameof(X)}: {X}, {nameof(Y)}: {Y}";
         return formattable.ToString(formatProvider);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public override string ToString()
     {
         return $"{nameof(X)}: {X}, {nameof(Y)}: {Y}";
--- tests/Euphoria.Math.Tests/Vector2Tests.cs
+++ tests/Euphoria.Math.Tests/Vector2Tests.cs
@@ -3,7 +3,7 @@
 public class Vector2Tests
 {
     [Test]
-    public void Construct()
+    public void TestConstruct()
     {
         Vector2T<int> vector = new Vector2T<int>(5, 10);
         
@@ -15,7 +15,7 @@ public class Vector2Tests
     }
     
     [Test]
-    public void ConstructScalar()
+    public void TestConstructScalar()
     {
         Vector2T<int> vector = new Vector2T<int>(5);
         
@@ -23,6 +23,19 @@ public class Vector2Tests
         {
             Assert.That(vector.X, Is.EqualTo(5));
             Assert.That(vector.Y, Is.EqualTo(5));
+        });
+    }
+
+    [Test]
+    public void TestCasting()
+    {
+        Vector2T<float> fVector = new Vector2T<float>(1.5f, 2.9f);
+        Vector2T<int> iVector = fVector.As<int>();
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(iVector.X, Is.EqualTo((int) fVector.X));
+            Assert.That(iVector.Y, Is.EqualTo((int) fVector.Y));
         });
     }
 
@@ -84,5 +97,70 @@ public class Vector2Tests
             Assert.That(c.X, Is.EqualTo(1 * 3));
             Assert.That(c.Y, Is.EqualTo(2 * 3));
         });
+    }
+
+    [Test]
+    public void TestDivision()
+    {
+        Vector2T<float> a = new Vector2T<float>(1, 2);
+        Vector2T<float> b = new Vector2T<float>(3, 4);
+
+        Vector2T<float> c = a / b;
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(c.X, Is.EqualTo(1f / 3f));
+            Assert.That(c.Y, Is.EqualTo(2f / 4f));
+        });
+    }
+
+    [Test]
+    public void TestDivisionScalar()
+    {
+        Vector2T<float> a = new Vector2T<float>(1, 2);
+        const float b = 3;
+
+        Vector2T<float> c = a / b;
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(c.X, Is.EqualTo(1f / 3f));
+            Assert.That(c.Y, Is.EqualTo(2f / 3f));
+        });
+    }
+
+    [Test]
+    public void TestEquals()
+    {
+        Vector2T<int> a = new Vector2T<int>(1, 2);
+        Vector2T<int> b = new Vector2T<int>(1, 2);
+
+        Assert.That(a == b, Is.True);
+        
+        Vector2T<int> c = new Vector2T<int>(3, 4);
+        
+        Assert.That(a == c, Is.False);
+    }
+
+    [Test]
+    public void TestNotEquals()
+    {
+        Vector2T<int> a = new Vector2T<int>(1, 2);
+        Vector2T<int> b = new Vector2T<int>(1, 2);
+
+        Assert.That(a != b, Is.False);
+        
+        Vector2T<int> c = new Vector2T<int>(3, 4);
+        
+        Assert.That(a != c, Is.True);
+    }
+
+    [Test]
+    public void TestToString()
+    {
+        Vector2T<int> vector = new Vector2T<int>(1, 2);
+        string str = vector.ToString();
+        
+        Assert.That(str, Is.EqualTo("X: 1, Y: 2"));
     }
 }
