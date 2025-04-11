56e2a2d15013a875e1e8ce38ad24cd055b92a5b7
Aqua <ollie@ollierobinson.co.uk>
2025-04-11 22:47:45 +0100

    transform struct

---
+++ src/Crimson.Engine/Scenes/Transform.cs
@@ -0,0 +1,39 @@
+using System.Numerics;
+
+namespace Crimson.Engine.Scenes;
+
+public struct Transform
+{
+    public Vector3 Position;
+
+    public Quaternion Rotation;
+
+    public Vector3 Scale;
+
+    public Vector3 Origin;
+
+    public Transform()
+    {
+        Position = Vector3.Zero;
+        Rotation = Quaternion.Identity;
+        Scale = Vector3.One;
+        Origin = Vector3.Zero;
+    }
+
+    public Vector3 Forward => Vector3.Transform(-Vector3.UnitZ, Rotation);
+
+    public Vector3 Backward => Vector3.Transform(Vector3.UnitZ, Rotation);
+
+    public Vector3 Up => Vector3.Transform(Vector3.UnitY, Rotation);
+
+    public Vector3 Down => Vector3.Transform(-Vector3.UnitY, Rotation);
+
+    public Vector3 Right => Vector3.Transform(Vector3.UnitX, Rotation);
+
+    public Vector3 Left => Vector3.Transform(-Vector3.UnitX, Rotation);
+
+    public Matrix4x4 WorldMatrix => Matrix4x4.CreateTranslation(-Origin) *
+                                    Matrix4x4.CreateScale(Scale) *
+                                    Matrix4x4.CreateFromQuaternion(Rotation) *
+                                    Matrix4x4.CreateTranslation(Position);
+}
