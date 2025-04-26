3c40a5d365f1eeecd703e910379dcebe7a877284
Aqua <ollie@ollierobinson.co.uk>
2025-04-26 15:00:13 +0100

    improve shader compiler to support dxil

--- src/Crimson.Graphics/Shaders/Debug/ImGui.hlsl
+++ src/Crimson.Graphics/Shaders/Debug/ImGui.hlsl
@@ -3,9 +3,9 @@
 
 struct VSInput
 {
-    float2 Position: POSITION0;
-    float2 TexCoord: TEXCOORD0;
-    float4 Color: COLOR0;
+    float2 Position: TEXCOORD0;
+    float2 TexCoord: TEXCOORD1;
+    float4 Color: TEXCOORD2;
 };
 
 struct VSOutput
--- tests/Crimson.Engine.Tests/Program.cs
+++ tests/Crimson.Engine.Tests/Program.cs
@@ -8,6 +8,6 @@ Logger.EnableConsole();
 
 AppOptions options = new AppOptions("Crimson.Engine.Tests", new Version(1, 0));
 options.Window.Resizable = true;
-options.Window.FullScreen = true;
+//options.Window.FullScreen = true;
 
 App.Run(in options, new TestScene());
