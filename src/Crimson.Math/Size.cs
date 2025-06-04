b82640d34eb62be22a8acbbc07d0eb0c2eb0e19a
Aqua <ollie@ollierobinson.co.uk>
2025-06-04 18:43:53 +0100

    size operators

--- src/Crimson.Math/Size.cs
+++ src/Crimson.Math/Size.cs
@@ -6,7 +6,17 @@ namespace Crimson.Math;
 /// A 2-dimensional size with a width and a height.
 /// </summary>
 /// <typeparam name="T"></typeparam>
-public readonly record struct Size<T> where T : INumber<T>
+public readonly struct Size<T> :
+    IEquatable<Size<T>>,
+    IAdditionOperators<Size<T>, Size<T>, Size<T>>,
+    ISubtractionOperators<Size<T>, Size<T>, Size<T>>,
+    IUnaryNegationOperators<Size<T>, Size<T>>,
+    IMultiplyOperators<Size<T>, Size<T>, Size<T>>,
+    IMultiplyOperators<Size<T>, T, Size<T>>,
+    IDivisionOperators<Size<T>, Size<T>, Size<T>>,
+    IDivisionOperators<Size<T>, T, Size<T>>,
+    IFormattable 
+    where T : INumber<T>
 {
     /// <summary>
     /// The width.
@@ -33,5 +43,72 @@ public readonly record struct Size<T> where T : INumber<T>
     /// Create a size with the given scalar.
     /// </summary>
     /// <param name="wh">The scalar width and height.</param>
-    public Size(T wh) : this(wh, wh) { }
+    public Size(T wh)
+    {
+        Width = wh;
+        Height = wh;
+    }
+    
+    /// <summary>
+    /// Cast this size to a <see cref="Size{T}"/> with a different component type.
+    /// </summary>
+    /// <typeparam name="TOther">A numeric type.</typeparam>
+    /// <returns>The cast size.</returns>
+    public Size<TOther> As<TOther>() where TOther : INumber<TOther>
+        => new Size<TOther>(TOther.CreateChecked(Width), TOther.CreateChecked(Height));
+
+    public static Size<T> operator +(Size<T> left, Size<T> right)
+        => new Size<T>(left.Width + right.Width, left.Height + right.Height);
+
+    public static Size<T> operator -(Size<T> left, Size<T> right)
+        => new Size<T>(left.Width - right.Width, left.Height - right.Height);
+
+    public static Size<T> operator -(Size<T> value)
+        => new Size<T>(-value.Width, -value.Height);
+
+    public static Size<T> operator *(Size<T> left, Size<T> right)
+        => new Size<T>(left.Width * right.Width, left.Height * right.Height);
+
+    public static Size<T> operator *(Size<T> left, T right)
+        => new Size<T>(left.Width * right, left.Height * right);
+
+    public static Size<T> operator /(Size<T> left, Size<T> right)
+        => new Size<T>(left.Width / right.Width, left.Height / right.Height);
+
+    public static Size<T> operator /(Size<T> left, T right)
+        => new Size<T>(left.Width / right, left.Height / right);
+
+    public bool Equals(Size<T> other)
+        => this == other;
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Size<T> other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Width, Height);
+    }
+
+    public static bool operator ==(Size<T> left, Size<T> right)
+    {
+        return left.Width == right.Width && left.Height == right.Height;
+    }
+
+    public static bool operator !=(Size<T> left, Size<T> right)
+    {
+        return left.Width != right.Width || left.Height != right.Height;
+    }
+
+    public string ToString(string? format, IFormatProvider? formatProvider)
+    {
+        FormattableString formattable = $"{Width}x{Height}";
+        return formattable.ToString(formatProvider);
+    }
+
+    public override string ToString()
+    {
+        return $"{Width}x{Height}";
+    }
 }
