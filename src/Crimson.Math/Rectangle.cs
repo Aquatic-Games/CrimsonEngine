e7c54ff136ed23c37f5da6007a65de58aa5a5cbe
Aqua <ollie@ollierobinson.co.uk>
2025-07-02 23:17:51 +0100

    add rectangle struct from RHI PR

---
+++ src/Crimson.Math/Rectangle.cs
@@ -0,0 +1,26 @@
+using System.Numerics;
+
+namespace Crimson.Math;
+
+public readonly struct Rectangle<T> where T : INumber<T>
+{
+    public readonly Vector2T<T> Position;
+
+    public readonly Size<T> Size;
+
+    public T X => Position.X;
+
+    public T Y => Position.Y;
+
+    public T Width => Size.Width;
+
+    public T Height => Size.Height;
+
+    public Rectangle(Vector2T<T> position, Size<T> size)
+    {
+        Position = position;
+        Size = size;
+    }
+
+    public Rectangle(T x, T y, T width, T height) : this(new Vector2T<T>(x, y), new Size<T>(width, height)) { }
+}
