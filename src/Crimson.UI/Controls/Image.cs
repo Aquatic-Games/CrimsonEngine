9e27b9f0e2d5d199657f185a2b5ca8e9468c279d
Aqua <ollie@ollierobinson.co.uk>
2025-07-20 23:44:02 +0100

    image tint

--- src/Crimson.UI/Controls/Image.cs
+++ src/Crimson.UI/Controls/Image.cs
@@ -1,18 +1,22 @@
 using Crimson.Graphics;
+using Crimson.Math;
 
 namespace Crimson.UI.Controls;
 
 public class Image : Control
 {
     public Texture Texture;
+
+    public Color Tint;
     
     public Image(Texture texture)
     {
         Texture = texture;
+        Tint = Color.White;
     }
     
     protected internal override void Draw()
     {
-        Renderer.DrawImage(Texture, ScreenRegion.Position, ScreenRegion.Size);
+        Renderer.DrawImage(Texture, ScreenRegion.Position, ScreenRegion.Size, tint: Tint);
     }
 }
