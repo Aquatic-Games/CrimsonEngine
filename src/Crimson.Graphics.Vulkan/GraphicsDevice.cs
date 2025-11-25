873c19ca30100a39fcb91fb5a1abd5f044e8690f
Aqua <ollie@ollierobinson.co.uk>
2025-11-25 21:52:48 +0000

    begin device creation

--- src/Crimson.Graphics.Vulkan/GraphicsDevice.cs
+++ src/Crimson.Graphics.Vulkan/GraphicsDevice.cs
@@ -1,5 +1,6 @@
 ﻿using Crimson.Core;
 using SDL3;
+using Silk.NET.Core;
 using Silk.NET.Core.Native;
 using Silk.NET.Vulkan;
 
@@ -9,6 +10,10 @@ public sealed unsafe class GraphicsDevice : IDisposable
 {
     private readonly Vk _vk;
     private readonly Instance _instance;
+
+    private readonly SurfaceKHR _surface;
+    
+    private readonly PhysicalDevice _physicalDevice;
 
     public GraphicsDevice(string appName, IntPtr sdlWindow)
     {
@@ -44,14 +49,44 @@ public sealed unsafe class GraphicsDevice : IDisposable
         
         Logger.Trace("Creating instance.");
         _vk.CreateInstance(&instanceInfo, null, out _instance).Check("Create instance");
-
+        
         SilkMarshal.Free(pInstanceExtensions);
         SilkMarshal.FreeString(pEngineName);
         SilkMarshal.FreeString(pAppName);
+
+        Logger.Trace("Creating surface.");
+        if (!SDL.VulkanCreateSurface(sdlWindow, _instance.Handle, IntPtr.Zero, out IntPtr surface))
+            throw new Exception($"Failed to create vulkan surface: {SDL.GetError()}");
+        _surface = new SurfaceKHR((ulong) surface);
+
+        Logger.Trace("Selecting physical device.");
+        uint numPhysicalDevices;
+        _vk.EnumeratePhysicalDevices(_instance, &numPhysicalDevices, null);
+        PhysicalDevice* physicalDevices = stackalloc PhysicalDevice[(int) numPhysicalDevices];
+        _vk.EnumeratePhysicalDevices(_instance, &numPhysicalDevices, physicalDevices);
+
+        // TODO: Better selection of physical device.
+        _physicalDevice = physicalDevices[0];
+
+        PhysicalDeviceProperties physicalDeviceProperties;
+        _vk.GetPhysicalDeviceProperties(_physicalDevice, &physicalDeviceProperties);
+        Version32 supportedVersion = (Version32) physicalDeviceProperties.ApiVersion;
+        
+        Logger.Info($"Using device: {new string((sbyte*) physicalDeviceProperties.DeviceName)}");
+        Logger.Info($"    Type: {physicalDeviceProperties.DeviceType}");
+        Logger.Info($"    Supported version: {supportedVersion.Major}.{supportedVersion.Minor}.{supportedVersion.Patch}");
+        
+        uint numQueueFamilies;
+        _vk.GetPhysicalDeviceQueueFamilyProperties(_physicalDevice, &numQueueFamilies, null);
+        QueueFamilyProperties* queueFamilyProperties = stackalloc QueueFamilyProperties[(int) numQueueFamilies];
+        _vk.GetPhysicalDeviceQueueFamilyProperties(_physicalDevice, &numQueueFamilies, queueFamilyProperties);
+        
+        _vk
     }
     
     public void Dispose()
     {
+        SDL.VulkanDestroySurface(_instance.Handle, (IntPtr) _surface.Handle, IntPtr.Zero);
         _vk.DestroyInstance(_instance, null);
         _vk.Dispose();
     }
