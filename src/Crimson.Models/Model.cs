e853632f5a29e46f5bcd271f2ad209df2b43e310
Aqua <ollie@ollierobinson.co.uk>
2025-12-12 17:37:42 +0000

    begin assimp integration

---
+++ src/Crimson.Models/Model.cs
@@ -0,0 +1,12 @@
+﻿using Assimp;
+
+namespace Crimson.Models;
+
+public class Model
+{
+    public Model(string path)
+    {
+        using AssimpContext context = new AssimpContext();
+        Scene scene = context.ImportFile(path, PostProcessSteps.Triangulate | PostProcessSteps.GenerateSmoothNormals);
+    }
+}
